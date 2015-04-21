2a83a1ca55f86a36d8659ce2148e30b145976b68
Amir Fayek <afayek88@gmail.com>
2015-04-21 14:12:04 -0700

    check box feature and delete

--- simple-todos.js
+++ simple-todos.js
@@ -35,6 +35,16 @@ if (Meteor.isClient) {
       // Prevent default form submit
       return false;
     }
-  })
+  });
 
+  Template.task.events({
+    "click .toggle-checked": function() {
+      Tasks.update(this._id, {$set: {checked: !this.checked}});
+    },
+
+    "click .delete": function() {
+      Tasks.remove(this._id);
+    }
+  });
 }
+
