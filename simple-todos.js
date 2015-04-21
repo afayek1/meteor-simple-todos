16140af3cc80afd2f5ea0bc4cd5332c5467a34e7
Amir Fayek <afayek88@gmail.com>
2015-04-21 13:42:21 -0700

    added mongodb

--- simple-todos.js
+++ simple-todos.js
@@ -1,13 +1,18 @@
+/*
+ * Collections store persistent data.
+ * Can be accessed from server and client.
+ * On client, creates cache connected to server collection.
+*/
+Tasks = new Mongo.Collection("tasks");
+
 if (Meteor.isClient) {
-  /*
-   * You can pass data into templates by defining helpers.
-   * This code will only run on the client.
-  */
+  // This code only runs on the client
   Template.body.helpers({
-    tasks: [
-      { text: "This is task 1" },
-      { text: "This is task 2" },
-      { text: "This is task 3" }
-    ]
+    tasks: function () {
+      return Tasks.find({});
+    }
   });
+
+  
+  
 }
