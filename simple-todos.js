4258f2d2113b97b97e60111d6bca27fca2dca91b
Amir Fayek <afayek88@gmail.com>
2015-04-21 15:54:39 -0700

    remaining tasks feature

--- simple-todos.js
+++ simple-todos.js
@@ -20,6 +20,10 @@ if (Meteor.isClient) {
 
     hideCompleted: function() {
       return Session.get("hideCompleted");
+    },
+
+    incompleteCount: function() {
+      return Tasks.find({checked: {$ne: true}}).count();
     }
   });
 
