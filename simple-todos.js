7e7a49ff4ec1a09e467b95136a87058dff4e5247
Amir Fayek <afayek88@gmail.com>
2015-04-21 13:47:24 -0700

    added form that submits to db and appends to list

--- simple-todos.js
+++ simple-todos.js
@@ -13,6 +13,23 @@ if (Meteor.isClient) {
     }
   });
 
-  
-  
+  Template.body.events({
+    "submit .new-task": function (event) {
+      // This function is called when new taks form is submitted
+
+      var text = event.target.text.value;
+
+      Tasks.insert({
+        text: text,
+        createdAt: new Date() // current time
+      });
+
+      // Clear form
+      event.target.text.value = "";
+
+      // Prevent default form submit
+      return false;
+    }
+  })
+
 }
