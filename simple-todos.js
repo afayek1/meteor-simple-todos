6717b6f785a6d832ea5d1981a2a65eac500f45a8
Amir Fayek <afayek88@gmail.com>
2015-04-21 21:45:31 -0700

    added private feature

--- simple-todos.js
+++ simple-todos.js
@@ -5,8 +5,12 @@
 */
 Tasks = new Mongo.Collection("tasks");
 
-if (Meteor.isClient) {
-  // This code only runs on the client
+
+// This code only runs on the client
+if (Meteor.isClient) {  
+  // When called with publication name, client subscibes to all data from that publication
+  Meteor.subscribe("tasks");
+
   Template.body.helpers({
     tasks: function () {
       if (Session.get("hideCompleted")) {
@@ -63,8 +67,18 @@ if (Meteor.isClient) {
 
     "click .delete": function() {
       Meteor.call("deleteTask", this._id);
+    },
+
+    "click .toggle-private" : function() {
+      Meteor.call("setPrivate", this._id, !this.private);
     }
   });
+
+  Template.task.helpers({
+    isOwner: function() {
+      return this.owner === Meteor.userId();
+    }
+  })
 
   Accounts.ui.config({
     passwordSignupFields: "USERNAME_ONLY"
@@ -73,7 +87,7 @@ if (Meteor.isClient) {
 
 Meteor.methods({
   addTask: function (text) {
-    if (! Meteor.UserId()) {
+    if (! Meteor.userId()) {
       throw new Meteor.Error("not-authorized")
     }
 
@@ -86,10 +100,42 @@ Meteor.methods({
   },
 
   deleteTask: function (taskId) {
+    var task = Tasks.findOne(taskId);
+    if (task.private && task.owner !== Meteor.userId()) {
+      throw new Meteor.Error("not-authorized");
+    }
     Tasks.remove(taskId);
   },
 
   setChecked: function (taskId, setChecked) {
+    var task = Tasks.findOne(taskId);
+    if (task.private && task.owner !== Meteor.userId()) {
+      throw new Meteor.error("not-authorized");
+    }
     Tasks.update(taskId, { $set: { checked: setChecked} });
+  },
+
+  setPrivate: function (taskId, setToPrivate) {
+    var task = Tasks.findOne(taskId);
+
+    if (task.owner !== Meteor.userId()) {
+      throw new Meteor.Error("not-authorized");
+    }
+
+    Tasks.update(taskId, { $set: {private: setToPrivate} });
   }
 });
+
+if (Meteor.isServer) {
+  /*
+   * Meteor.publish registers a publication named "tasks"
+  */
+  Meteor.publish("tasks", function () {
+    return Tasks.find({
+      $or: [
+        { private: {$ne: true} },
+        { owner: this.userId }
+      ]
+    });
+  });
+}
